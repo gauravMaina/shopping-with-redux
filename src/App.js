0777c5aeb28c3c3210b7828c95a2a51a03e06ed0
gaurav.jadhav <gaurav.jadhav@octalinfosolution.com>
2023-07-20 13:03:12 +0530

    action create and call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { useDispatch } from 'react-redux'
+import { addToCart } from './redux/action'
 
-function App() {
+function App () {
+  const dispatch = useDispatch()
+  const product = {
+    model: 'I phone 11 pro',
+    // type: 'mobile',
+    price: '50000'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='text-center'>
+      <button onClick={() => dispatch(addToCart(product))}>Add to cart</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
